f5a713ebe80aa6eed7d828955392ce2bf9b34e43
Davi Scripts <daviscripts07@gmail.com>
2026-04-13 18:08:14 -0300

    Fix URL in loadstring for DaviScripts.lua

--- DaviScripts.lua
+++ DaviScripts.lua
@@ -1,3 +1,3 @@
 script_key = "KEY"; -- A key might be required, if not, delete this line.
-loadstring(game:HttpGet("https://raw.githubusercontent.com/davi-scripts-hub/davi-scripts-hub/refs/heads/main/DaviScripts.luahttps://raw.githubusercontent.com/davi-scripts-hub/davi-scripts-hub/refs/heads/main/DaviScripts.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/davi-scripts-hub/davi-scripts-hub/refs/heads/main/DaviScripts.lua"))()
  
