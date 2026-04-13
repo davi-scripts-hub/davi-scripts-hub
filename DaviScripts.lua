46e4bf134a88dbe06a14c990856ca1f989f18c8b
Davi Scripts <daviscripts07@gmail.com>
2026-04-13 18:05:31 -0300

    Update script loading URL in DaviScripts.lua

--- DaviScripts.lua
+++ DaviScripts.lua
@@ -1,3 +1,3 @@
 script_key = "KEY"; -- A key might be required, if not, delete this line.
-loadstring(game:HttpGet(""))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/davi-scripts-hub/davi-scripts-hub/refs/heads/main/DaviScripts.lua"))()
  
