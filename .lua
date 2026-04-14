c61348ade208452013e11278a09d193a679a6651
Davi Scripts <daviscripts07@gmail.com>
2026-04-14 16:41:11 -0300

    Add .lua to .gitignore

---
+++ .lua
@@ -0,0 +1,27 @@
+IS_CUSTOM_UI_MODE = true
+CUSTOM_UI_MODE_DATA = {
+    Name = "Davi",
+    Theme = "darkblue",
+    Logo = "rbxassetid://96012102609548",
+    Background = "rbxassetid://98096100327291",
+    YouTube = "DaviScripts",
+    Discord = "vh8hKNTc4",
+    WelcomeMessage = {
+        "Davi Scripts HUB",
+        "BY Davi Scripts"
+    },
+    TrailIcon = "💙",
+    ToggleIcon = "rbxassetid://88870369034630",
+    CommunityIcon = "rbxassetid://129698128709234",
+    Contributers = {
+        "Davi Scripts | hashishx"
+    },
+    IntroText = "Acredite em si mesmo e você será imparável.",
+
+    GetKeyLink = "https://direct-link.net/5038977/MyrmI39cr5La",
+    TutorialLink = "https://direct-link.net/5038977/MyrmI39cr5La"
+}
+
+_G.MY_KEY_IS = "https://direct-link.net/5038977/MyrmI39cr5La"
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/1c3b3c8a311a71c8e81c44b998a81e39.lua"))()
